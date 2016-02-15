bb3aa6107a5d3007e20bc485681fc288ea9d05ca
maps90 <dimas.abdulaziiz@mataharimall.com>
2016-02-15 17:39:49 +0700

    added several test case.

--- tests/MataharimallApiTest.php
+++ tests/MataharimallApiTest.php
@@ -3,20 +3,70 @@
 namespace Mataharimall;
 
 use Mataharimall\API;
+use Mataharimall\MMRequest;
 
-class ApiTest extends \PHPUnit_Framework_TestCase
+class MataharimallApiTest extends \PHPUnit_Framework_TestCase
 {
     protected $MM;
+    protected $result;
 
     protected function setup()
     {
         $this->MM = new API(API_TOKEN);
+        //test POST API
+        $this->result = $this->MM->post('master/brands', [
+            'page' => 1,
+            'limit' => 5,
+        ]);
     }
 
-    public function testGetMasterBrands()
+    public function testGetLastHttpCode()
     {
-        $result = $this->MM->post('master/brands', array('page' => 1,'limit' => 5));
         $this->assertEquals(200, $this->MM->getLastHttpCode());
-        return;
     }
+
+    public function testGetLastHeaders()
+    {
+        $headers = $this->MM->getLastHeaders();
+        $this->assertInternalType('array', $headers);
+    }
+
+    public function testGetLastBody()
+    {
+        $body = $this->MM->getLastBody();
+        $this->assertArrayHasKey('code', $body);
+        $this->assertArrayHasKey('results', $body);
+        foreach ($body as $key => $value) {
+            if ($key == 'code') {
+                $this->assertEquals(200, $value);
+            }
+        }
+    }
+
+    public function testEmptyToken()
+    {
+        $this->setExpectedException('Mataharimall\MMException');
+        try {
+            $this->MM = new API();
+            $this->result = $this->MM->post('master/colors', []);
+         } catch (Mataharimall\MMException $e) {
+             $this->assertContains('Invalid API token.', $e->getMessage());
+             throw $e;
+         }
+    }
+
+    public function testCurlProxy()
+    {
+        $request = new MMRequest([
+            'CURLOPT_PROXY' => PROXY_HOST,
+            'CURLOPT_PROXYPORT' => PROXY_PORT,
+        ]);
+        $this->MM = new API(API_TOKEN, $request);
+        $result = $this->MM->post('master/colors', []);
+        $fields = $request->getCurlOptions();
+        $this->assertEquals($fields[CURLOPT_PROXY], PROXY_HOST);
+
+    }
+
+
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,5 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 define('API_TOKEN', getenv('TEST_API_TOKEN'));
+define('PROXY_HOST', getenv('TEST_PROXY_HOST'));
+define('PROXY_PORT', getenv('TEST_PROXY_PORT'));
